5a0174ac39bdecde60e1092c4ed008d4d4c7a6d0
Javier Rivera Romero <javierri.rivera@gmail.com>
2016-02-27 19:56:34 -0430

    Update MetodoInsercion.py

--- MetodoInsercion.py
+++ MetodoInsercion.py
@@ -6,7 +6,8 @@ arreglo = [5,3,4,2,6,1,7,9,3,8,6]
 
 i = 1
 for elem in arreglo[1:]:
-    for j in range(i-1,-1,-1):
+    j = i - 1
+    while (j >= 0):
         if (arreglo[j] < elem):
             del arreglo[i]
             arreglo.insert(j+1,elem)
@@ -15,6 +16,8 @@ for elem in arreglo[1:]:
         if (j == 0 and arreglo[j] >= elem):
             del arreglo[i]
             arreglo.insert(0,elem)
+            
+        j = j - 1
            
     i = i + 1
 

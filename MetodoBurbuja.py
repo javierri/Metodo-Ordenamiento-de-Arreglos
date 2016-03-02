5bbfafa60e87920bb40e5b31742d7771b68e50d7
Javier Rivera Romero <javierri.rivera@gmail.com>
2016-03-01 21:14:37 -0430

    Update MetodoBurbuja.py

--- MetodoBurbuja.py
+++ MetodoBurbuja.py
@@ -2,20 +2,20 @@
 # Observación: Método mejorado
 # Autor: Javier Rivera (UNEFA Mérida)
 
+def OrdBurbuja():
+    Ordenado = False
+    while (Ordenado == False):
+        i = 0
+        Ordenado = True
+        for elem in arreglo[:len(arreglo)-1]:
+            if (elem > arreglo[i+1]):
+                Ordenado = False
+                aux = arreglo[i+1]
+                del arreglo[i+1]
+                arreglo.insert (i,aux)            
+            i = i + 1
+
 arreglo = [2,4,1,7,5,3,8,9,6]
 print arreglo
-
-Ordenado = False
-while (Ordenado == False):
-    i = 0
-    Ordenado = True
-    for elem in arreglo[:len(arreglo)-1]:
-        if (elem > arreglo[i+1]):
-            Ordenado = False
-            aux = arreglo[i+1]
-            del arreglo[i+1]
-            arreglo.insert (i,aux)            
-                      
-        i = i + 1
-        
+OrdBurbuja(arreglo)
 print arreglo

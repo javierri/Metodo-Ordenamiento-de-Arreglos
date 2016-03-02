c87043c31835fc45b3924983906e457cb296252f
Javier Rivera Romero <javierri.rivera@gmail.com>
2016-03-01 21:23:07 -0430

    Update MetodoSeleccion.py

--- MetodoSeleccion.py
+++ MetodoSeleccion.py
@@ -2,17 +2,19 @@
 # Observación: Solo funciona si el arreglo no tiene elementos repetidos
 # Autor: Javier Rivera (UNEFA Mérida)
 
+def OrdSeleccion:
+    menor = arreglo[0]
+    for i in range(len(arreglo)):
+        menor = arreglo[i]
+        for elem in arreglo[i:]:
+            if (elem < menor):
+                menor = elem
+        
+        if (menor < arreglo[i]):
+            arreglo.remove(menor)
+            arreglo.insert(i,menor)
+
 arreglo = [5,3,4,2,6,1,7,9,8]
-
-menor = arreglo[0]
-for i in range(len(arreglo)):
-    menor = arreglo[i]
-    for elem in arreglo[i:]:
-        if (elem < menor):
-            menor = elem
-    
-    if (menor < arreglo[i]):
-        arreglo.remove(menor)
-        arreglo.insert(i,menor)
-
+print arreglo
+OrdSeleccion(arreglo)
 print arreglo

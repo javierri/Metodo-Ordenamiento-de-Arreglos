3e7cbcbe08a1f5970658fcc09c8c480d48b5e7ac
Javier Rivera Romero <javierri.rivera@gmail.com>
2016-03-10 13:06:01 -0430

    Actividad
    Crear un Branch con el desarrollo de un método de de ordenamiento QuickSort donde el pivote este en el centro. (Para crear el branch debe cambiar la opción de abajo (los checkbox) de commit a new branch )
    
    Solo un miembro del grupo podrá realizar el branch y deberá solicitar autorización por correo para realizarlo.(Cada grupo debe crear un solo branch)

---
+++ MetodoQuickSort_pc.py
@@ -0,0 +1,14 @@
+
+# Metodo de Ordenamiento por Selección 
+# Observación: Pivote en el centro
+# Autores: 
+
+def OrdQuickSort(arreglo):
+  pass
+  
+# PRINCIPAL
+
+arreglo = [4,6,2,8,3,5,1,7,9,5]
+print arreglo
+arreglo = OrdQuickSort(arreglo)        
+print arreglo

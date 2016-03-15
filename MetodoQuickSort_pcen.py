1b76918300587a4c96a2ebfcf073f305a3a31651
Javier Rivera Romero <javierri.rivera@gmail.com>
2016-03-14 21:02:37 -0430

    Create MetodoQuickSort_pcen.py

---
+++ MetodoQuickSort_pcen.py
@@ -0,0 +1,13 @@
+# Metodo de Ordenamiento por Selección 
+# Observación: Pivote al Centro
+# Autores: 
+
+def OrdQuickSort(arreglo):
+  pass
+  
+# PRINCIPAL
+
+arreglo = [4,6,2,8,3,5,1,7,9,5]
+print arreglo
+arreglo = OrdQuickSort(arreglo)        
+print arreglo

66dd1bb5748198a0cd8a84b939cfa52d3747096b
Javier Rivera Romero <javierri.rivera@gmail.com>
2016-03-15 11:09:33 -0430

    Create MetodoInsercionBinaria.py

---
+++ MetodoInsercionBinaria.py
@@ -0,0 +1,38 @@
+# Busqueda Binaria
+def buscar(arreglo, elem):
+    
+    pi = 0
+    pf = len(arreglo) - 1
+    pm = 0
+    
+    while (pi <= pf):
+        
+        pm = (pi + pf)/2
+        if (elem == arreglo[pm]):
+            return pm 
+        if (elem < arreglo[pm]):
+            pf = pm - 1
+        else:
+            pi = pm + 1
+
+    if (len(arreglo) > pm and elem > arreglo[pm]):
+        return pm + 1
+        
+    return pm 
+    
+# Ordenamiento por Inserción Binaria
+def OrdBinario(arreglo):
+    arregloOrd = []
+    
+    for i in range(len(arreglo)):
+        pos = buscar(arregloOrd, arreglo[i])
+        arregloOrd.insert(pos, arreglo[i])
+        del arreglo[0]
+        
+    return arregloOrd
+
+# PRINCIPAL
+    
+a = [6,4,1,9,2,8,3,5,7,12,0,11]
+a = OrdBinario(a)
+print a

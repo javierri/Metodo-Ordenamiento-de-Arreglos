8be6245514fccbb29893e061650bb2a0adf6395e
Javier Rivera Romero <javierri.rivera@gmail.com>
2016-04-03 19:32:32 -0430

    Update MetodoQuickSort_pizq.py
    El pivote no esta a la izquierda, está a la derecha .. no es lo solicitado.. el while de las lineas 17 y 18 nunca ingresa porque pivote siempre es el último elemento y nunca es mayor al tamaño de la  lista.. el ejercicio practicamente es igual al visto en clases. El ejercicio es exactamente igual al branche #11

--- MetodoQuickSort_pizq.py
+++ MetodoQuickSort_pizq.py
@@ -1,7 +1,8 @@
 # Metodo de Ordenamiento por Selección 
 # Observación: Pivote a la Izquierda
 # Autores: Roentgen Pereira, Yorbelis Davila
-https://repl.it/Bvgl/14
+# https://repl.it/Bvgl/14
+
 def OrdQuickSort(arreglo):
     #condicion de parada
     if (len(arreglo)<=1):
@@ -23,8 +24,7 @@ def OrdQuickSort(arreglo):
     a2 = OrdQuickSort(arreglo[pivote+1:])
     
     return a1 + [arreglo[pivote]] + a2
-        
-        
+
 # PRINCIPAL
 print "Ejercicio de método Quick Sort presestado por:"
 print "Roentgen Pereira, Yorbelis Davila"
